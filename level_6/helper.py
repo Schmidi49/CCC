0f3430894bbe84d57213b34522f128a057b3a5cd
Erik Schmidthaler <erik.schmidi49@gmail.com>
2025-11-14 17:48:48 +0100

    alternate mode

--- level_6/helper.py
+++ level_6/helper.py
@@ -1,18 +1,19 @@
-def get_alt_waypoints(ast_x, ast_y, ss_x, ss_y):
+def get_alt_waypoints(ast_x, ast_y, ss_x, ss_y, complemetary = True):
     new_waypoints = [[ast_x, ast_y], [ast_x, ast_y]]
+    inc = 3 if complemetary else -3
 
     if (abs(ast_y) < abs(ast_x)):
-        new_waypoints[0][0] += 3 if (0 > ast_x) else -3
-        new_waypoints[0][1] += 3 if (ss_y > 0) else -3
+        new_waypoints[0][0] += inc if (0 > ast_x) else -inc
+        new_waypoints[0][1] += inc if (ss_y > 0) else -inc
 
-        new_waypoints[1][0] += -3 if (0 > ast_x) else 3
-        new_waypoints[1][1] += 3 if (ss_y > 0) else -3
+        new_waypoints[1][0] += -inc if (0 > ast_x) else inc
+        new_waypoints[1][1] += inc if (ss_y > 0) else -inc
 
     else:
-        new_waypoints[0][0] += 3 if (0 > ast_x) else -3
-        new_waypoints[0][1] += -3 if (ss_y > 0) else 3
+        new_waypoints[0][0] += inc if (0 > ast_x) else -inc
+        new_waypoints[0][1] += -inc if (ss_y > 0) else inc
 
-        new_waypoints[1][0] += 3 if (0 > ast_x) else -3
-        new_waypoints[1][1] += 3 if (ss_y > 0) else -3
+        new_waypoints[1][0] += inc if (0 > ast_x) else -inc
+        new_waypoints[1][1] += inc if (ss_y > 0) else -inc
 
-    return new_waypoints
+    return new_waypoints if complemetary else [new_waypoints[1], new_waypoints[0]]
