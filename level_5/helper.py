c8e27a6cebe8b3f039156bf5956633ddc3070706
Erik Schmidthaler <erik.schmidi49@gmail.com>
2025-11-14 17:15:13 +0100

    correction

--- level_5/helper.py
+++ level_5/helper.py
@@ -4,7 +4,7 @@ def get_alt_waypoints(ast_x, ast_y, ss_x, ss_y):
     new_waypoints[0][0] += 3 if (0 > ast_x) else -3
     new_waypoints[0][1] += 3 if (ss_y > 0) else -3
 
-    new_waypoints[1][0] += 3 if (0 < ast_x) else -3
+    new_waypoints[1][0] += -3 if (0 > ast_x) else 3
     new_waypoints[1][1] += 3 if (ss_y > 0) else -3
 
     return new_waypoints
