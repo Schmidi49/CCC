cbda19f21188755fb480551d089c05609ded4071
Erik Schmidthaler <erik.schmidi49@gmail.com>
2025-11-14 16:58:55 +0100

    new waypoint helper

---
+++ level_5/helper.py
@@ -0,0 +1,10 @@
+def get_alt_waypoints(ast_x, ast_y, ss_x, ss_y):
+    new_waypoints = [[ast_x, ast_y], [ast_x, ast_y]]
+
+    new_waypoints[0][0] += 3 if (0 > ast_x) else -3
+    new_waypoints[0][1] += 3 if (0 > ast_y) else -3
+
+    new_waypoints[1][0] += 3 if (ss_x > ast_x) else -3
+    new_waypoints[1][1] += 3 if (ss_y > ast_y) else -3
+
+    return new_waypoints
