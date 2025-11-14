d4e588b038c35bd2b29f795b2da3105cdc73e39d
Erik Schmidthaler <erik.schmidi49@gmail.com>
2025-11-14 18:32:59 +0100

    more modes

--- level_6/helper.py
+++ level_6/helper.py
@@ -1,6 +1,6 @@
-def get_alt_waypoints(ast_x, ast_y, ss_x, ss_y, normal = True):
+def get_alt_waypoints(ast_x, ast_y, ss_x, ss_y, mode = 0):
     new_waypoints = [[ast_x, ast_y], [ast_x, ast_y]]
-    inc = 3 if normal else -3
+    inc = 3 if mode != 1 else -3
 
     if (abs(ast_y) < abs(ast_x)):
         new_waypoints[0][0] += inc if (0 > ast_x) else -inc
@@ -16,4 +16,9 @@ def get_alt_waypoints(ast_x, ast_y, ss_x, ss_y, normal = True):
         new_waypoints[1][0] += inc if (0 > ast_x) else -inc
         new_waypoints[1][1] += inc if (ss_y > 0) else -inc
 
-    return new_waypoints if normal else [new_waypoints[1], new_waypoints[0]]
+    if mode == 0:
+        return new_waypoints
+    if mode == 1:
+        return [new_waypoints[1][0], new_waypoints[0][0]]
+    else:
+        return [new_waypoints[1][0] + new_waypoints[0][0] / 2, new_waypoints[1][1] + new_waypoints[0][1] / 2]
