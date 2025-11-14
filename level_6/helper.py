6928f2a82bdee708ac4e2a8472b1abce0a240d19
Erik Schmidthaler <erik.schmidi49@gmail.com>
2025-11-14 18:40:46 +0100

    mode 4

--- level_6/helper.py
+++ level_6/helper.py
@@ -1,6 +1,6 @@
 def get_alt_waypoints(ast_x, ast_y, ss_x, ss_y, mode = 0):
     new_waypoints = [[ast_x, ast_y], [ast_x, ast_y]]
-    inc = 3 if mode != 1 else -3
+    inc = 3 if (mode % 2 == 0) else -3
 
     if (abs(ast_y) < abs(ast_x)):
         new_waypoints[0][0] += inc if (0 > ast_x) else -inc
